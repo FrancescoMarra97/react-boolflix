fc37ba13a17c7da68596e4ec38edbb09e52eb371
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-28 12:23:29 +0100

    pt1 searchbar

--- react-boolflix/src/App.jsx
+++ react-boolflix/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { GlobalContext } from './context/GlobalContext'
-
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppFooter from './components/AppFooter'
 
 function App() {
 
 
   return (
     <>
+
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
 
     </>
   )
--- react-boolflix/src/components/SearchBar.jsx
+++ react-boolflix/src/components/SearchBar.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react"
+
+
 export default function SearchBar() {
+    //definiamo lo stato dell'input
 
+    const [query, setQuery] = useState("")
 
+    function handleInput(e) {
+        console.log(e);
+        setQuery(e.target.value)
+    }
+
+    function handleClick() {
+        console.log(`stai cercando ${query}`);
+        //in questa funzione fare la chiamata ajax per cercare i film
+    }
     return (
         <>
-
+            <div>
+                <input
+                    type="search"
+                    name=""
+                    value={query}
+                    placeholder="Cerca un Film"
+                    onChange={handleInput}
+                />
+                <button onClick={handleClick}>Cerca</button>
+            </div>
         </>
     )
 }
