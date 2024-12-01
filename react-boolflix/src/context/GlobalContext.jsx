919e918ce70a736999e001dbea1708015542bad2
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-01 17:55:14 +0100

    file .env

--- react-boolflix/src/context/GlobalContext.jsx
+++ react-boolflix/src/context/GlobalContext.jsx
@@ -10,13 +10,15 @@ export const GlobalProvider = ({ children }) => {
         setQuery(e.target.value);
     };
     const handleClick = () => {
-        const apiKey = "94fbc0828d753da23258202acf3bb239";
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`)
+        const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY
+
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`)
             .then((res) => res.json())
             .then((data) => setMovies(data.results))
             .catch((err) => console.log(err));
 
-        fetch(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}`)
+        fetch(`https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${query}`)
             .then((res) => res.json())
             .then((data) => setTvShows(data.results))
             .catch((err) => console.log(err));
